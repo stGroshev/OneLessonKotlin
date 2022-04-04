69ecfa4035155fdf5c794d02b67c7f0591694748
stGroshev <st.groshev96@gmail.com>
2022-04-04 16:06:35 +0300

    Lesson 1. Пункты 1-3 из дз: создал проект на java и перевел его на Kotlin, MainActivity.java конвертировал в MainActivity.kt через студию.

---
+++ app/src/main/java/com/geekbrains/groshev/stepan/onelessonkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geekbrains.groshev.stepan.onelessonkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geekbrains.groshev.stepan.onelessonkotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
