9dd2b3adf0615d0e1d9c06b3e6ced82a8eec7d16
stGroshev <st.groshev96@gmail.com>
2022-04-04 17:31:44 +0300

    Lesson 1. Пункт 4 из ДЗ: добавил кнопку, повесил на нее setOnClickListener, добавил функцию для отображения смайлов, было интересно, как работать с кодировкой unicode.

--- app/src/main/java/com/geekbrains/groshev/stepan/onelessonkotlin/MainActivity.kt
+++ app/src/main/java/com/geekbrains/groshev/stepan/onelessonkotlin/MainActivity.kt
@@ -2,11 +2,19 @@ package com.geekbrains.groshev.stepan.onelessonkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.geekbrains.groshev.stepan.onelessonkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener{
+            textView.text = "Вы нажали кнопку " + getEmojiByUnicode(0x1F609)
+        }
     }
 }
+fun getEmojiByUnicode(unicode: Int): String = String(Character.toChars(unicode)) // нашел данную функцию на stackoverflow
