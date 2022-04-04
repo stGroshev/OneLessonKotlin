8dd7c38ca3b555c3b0187d1bee9a0e4bf3c479b3
stGroshev <st.groshev96@gmail.com>
2022-04-04 22:02:31 +0300

    Lesson 1. Пункт 5a, 5b из ДЗ: Создал data class TestData.kt, вывел  его свойства в TextView, создал object TestTwo и сделал его копию, которую положил в testThree и вывел их свойства в TextView. + поигрался с операторм when, чтоб легкую красоту навести в App.

--- app/src/main/java/com/geekbrains/groshev/stepan/onelessonkotlin/MainActivity.kt
+++ app/src/main/java/com/geekbrains/groshev/stepan/onelessonkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.geekbrains.groshev.stepan.onelessonkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.geekbrains.groshev.stepan.onelessonkotlin.R
@@ -10,11 +11,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.textView)
         val button: Button = findViewById(R.id.button)
-        button.setOnClickListener{
-            textView.text = "Вы нажали кнопку " + getEmojiByUnicode(0x1F609)
+        val textView: TextView = findViewById(R.id.textView)
+        val textViewTwo: TextView = findViewById(R.id.textViewTwo)
+        val textViewParagraph: TextView = findViewById(R.id.textViewParagraph)
+        var count = 0
+        button.setOnClickListener {
+            count++
+            when (count) {
+                1 -> {
+                    textViewParagraph.text = "Пункт 5a"
+                    textView.text = "Вы нажали кнопку " + getEmojiByUnicode(0x1F609)
+                    val test = TestData("Андрей", "Нестеренко")
+                    textViewTwo.text = test.name + " " + test.surname + ","
+                    button.text = "Далее"
+                }
+                2 -> {
+                    textViewParagraph.text = "Пункт 5b"
+                    textViewTwo.text =
+                        "Свойства объекта: a = " + TestTwo.a.toString() + "; b = " + TestTwo.b.toString() + "."
+                    val testThree = TestTwo.copy()
+                    textView.text =
+                        "Свойства клона: a = " + testThree.a.toString() + "; b = " + testThree.b.toString() + "."
+                }
+                else -> {
+                    textViewParagraph.text = ""
+                    textViewTwo.text = ""
+                    textView.text = "Пункты из дз закончились" + getEmojiByUnicode(0x1F622)
+                    button.text = "Конец"
+                }
+            }
         }
     }
+
+    object TestTwo {
+        val a = 20
+        val b = 30
+        fun copy(a: Int = this.a, b: Int = this.b) = TestTwo
+    }
 }
-fun getEmojiByUnicode(unicode: Int): String = String(Character.toChars(unicode)) // нашел данную функцию на stackoverflow
+
+fun getEmojiByUnicode(unicode: Int): String =
+    String(Character.toChars(unicode)) // нашел эту функцию на stackoverflow
+
---
+++ app/src/main/java/com/geekbrains/groshev/stepan/onelessonkotlin/TestData.kt
@@ -0,0 +1,3 @@
+package com.geekbrains.groshev.stepan.onelessonkotlin
+
+data class TestData(val name: String, val surname: String)
